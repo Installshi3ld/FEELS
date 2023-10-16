23cf645d9d798a4c760fc836b79b465de25607de
Installshi3ld <thomas.wawrysko@gmail.com>
2023-10-16 15:02:46 +0200

    Regex json sorting

--- Assets/User/Thomas/GoogleSheetToUnity/GetGoogleSheetData.cs
+++ Assets/User/Thomas/GoogleSheetToUnity/GetGoogleSheetData.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
-
 
 public class GetGoogleSheetData : MonoBehaviour
 {
@@ -10,29 +12,41 @@ public class GetGoogleSheetData : MonoBehaviour
 
     //https://sheets.googleapis.com/v4/spreadsheets/1SCV0KSFDv43Ju-laybj3DhIx9bNrsv_pOXKl9is59o8/values/test?alt=json&key=AIzaSyB3aEK_MB0SlLidbcfccoMvpLi6g3GmJiM
 
-    List<List<string>> data;
+
+    //  regex =  \[([^\]\[]*)\]
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         StartCoroutine(ObtainSheetData());
 
         
     }
 
+    List<List<string>> jsonElement = new List<List<string>>();
     IEnumerator ObtainSheetData()
     {
         string json;
         UnityWebRequest www = UnityWebRequest.Get("https://sheets.googleapis.com/v4/spreadsheets/1SCV0KSFDv43Ju-laybj3DhIx9bNrsv_pOXKl9is59o8/values/test?alt=json&key=AIzaSyB3aEK_MB0SlLidbcfccoMvpLi6g3GmJiM");
         yield return www.SendWebRequest();
-        if(www.isNetworkError || www.isHttpError)
+        if (www.isNetworkError || www.isHttpError)
         {
             print("ERROR" + www.error);
         }
         else
         {
             json = www.downloadHandler.text;
-            Debug.Log(json);
+
+            MatchCollection regexSort = Regex.Matches(json, "\\[([^\\]\\[]*)\\]");
+
+            foreach (Match item in regexSort)
+            {
+                List<string> tmpList = new List<string>();
+
+                string resultat = Regex.Replace(item.ToString(), @"[\n\[\]]|\s*[\""]*", "").Trim();
+                print(resultat);
+                //jsonElement[i].AddRange(resultat.Split(',').ToList());
+            }
 
         }
     }
