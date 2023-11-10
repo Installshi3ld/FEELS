a2de154a5e54ee7c4ccfaa04a4f8af4da180d8dc
Installshi3ld <61904137+Installshi3ld@users.noreply.github.com>
2023-11-10 16:43:29 +0100

    Tool Editor hierachy + Sort main scene

---
+++ Assets/Editor/S_CustomIcon.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Hierarchy window game object icon.
+/// http://diegogiacomelli.com.br/unitytips-hierarchy-window-gameobject-icon/
+/// </summary>
+[InitializeOnLoad]
+public static class HierarchyWindowGameObjectIcon
+{
+    const string IgnoreIcons = "d_GameObject Icon, d_Prefab Icon, sv_label_0";
+
+    static HierarchyWindowGameObjectIcon()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyWindowItemOnGUI;
+
+        //EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCallback;
+    }
+
+    static void HandleHierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        var obj = EditorUtility.InstanceIDToObject(instanceID);
+
+        var content = EditorGUIUtility.ObjectContent(EditorUtility.InstanceIDToObject(instanceID), null);
+
+        if (content.image != null && !IgnoreIcons.Contains(content.image.name))
+        {
+            GUI.DrawTexture(new Rect(selectionRect.xMax - 16, selectionRect.yMin, 16, 16), content.image);
+            Debug.Log(content.image.name);
+        }
+        else if (content.image != null && content.image.name == "sv_label_0")
+        {
+            Rect offsetRect = new Rect(selectionRect.position, selectionRect.size);
+            EditorGUI.DrawRect(selectionRect, Color.white);
+            EditorGUI.LabelField(offsetRect, obj.name, new GUIStyle()
+            {
+                normal = new GUIStyleState() { textColor = Color.black },
+                fontStyle = FontStyle.Bold
+            });
+        }
+
+    }
+
+}
