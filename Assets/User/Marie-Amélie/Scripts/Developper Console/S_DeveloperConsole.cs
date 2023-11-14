ba08cb2f9d66c5ee8885f0213a622e94086da72d
DeepCalvaire <bellecourmarieamelie@gmail.com>
2023-11-14 09:01:04 +0100

    Simple Developer console system working
    pausing time + UI

---
+++ Assets/User/Marie-Amélie/Scripts/Developper Console/S_DeveloperConsole.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class S_DeveloperConsole
+{
+    private readonly string prefix;
+    private readonly IEnumerable<IConsoleCommand> commands;
+    public S_DeveloperConsole(string prefix, IEnumerable<IConsoleCommand> commands) 
+    {
+        this.prefix = prefix;
+        this.commands = commands;
+    }
+
+    public void ProcessCommand(string inputValue)
+    {
+        if (!inputValue.StartsWith(prefix))
+        {
+            return;
+        }
+
+        inputValue = inputValue.Remove(0, prefix.Length);
+
+        string[] inputSplit = inputValue.Split(' ');
+        
+        string commandInput = inputSplit[0];
+        string[] args = inputSplit.Skip(1).ToArray();
+
+        ProcessCommand(commandInput, args);
+    }
+    public void ProcessCommand(string commandInput, string[] args) 
+    {
+        foreach (var command in commands)
+        {
+            if(!commandInput.Equals(command.CommandWord, System.StringComparison.OrdinalIgnoreCase))
+            {
+                continue;
+            }
+            if(command.Processed(args))
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/User/Marie-Amélie/Scripts/Developper Console/S_DeveloperConsoleBehaviour.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class S_DeveloperConsoleBehavior : MonoBehaviour
+{
+    [SerializeField] private string prefix = string.Empty;
+    [SerializeField] private S_ConsoleCommand[] commands = new S_ConsoleCommand[0];
+
+    [Header("UI")]
+    [SerializeField] private GameObject UICanvas;
+    [SerializeField] private TMP_InputField inputField = null;
+
+    private float pausedTimeScale;
+
+    private S_DeveloperConsole developerConsole;
+    private static S_DeveloperConsoleBehavior instance;
+
+    private S_DeveloperConsole DeveloperConsole
+    {
+        get
+        {
+            if(developerConsole != null) { return developerConsole; }
+            return developerConsole = new S_DeveloperConsole(prefix, commands);
+
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance!=null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Toggle(CallbackContext context)
+    {
+        if(!context.action.triggered)
+        {
+            return;
+        }
+
+        if (UICanvas.activeSelf)
+        {
+            Time.timeScale = pausedTimeScale;
+            UICanvas.SetActive(false);
+        }
+        else
+        {
+            pausedTimeScale = Time.timeScale;
+            Time.timeScale = 0f;
+            UICanvas.SetActive(true);
+            inputField.ActivateInputField();
+        }
+
+    }
+
+    public void ProcessCommand(string inputValue)
+    {
+        DeveloperConsole.ProcessCommand(inputValue);
+
+        inputField.text = string.Empty;
+    }
+
+
+}
