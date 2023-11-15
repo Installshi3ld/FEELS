5e16bc447c9c0ca0a6a0324ecdf1954b5192e4c5
DeepCalvaire <bellecourmarieamelie@gmail.com>
2023-11-15 15:17:52 +0100

    Console UI
    Adding hour + date on debug messages
    Adding colors and UI system for messages

--- Assets/User/Marie-Amélie/Scripts/Developper Console/S_UIConsoleText.cs
+++ Assets/User/Marie-Amélie/Scripts/Developper Console/S_UIConsoleText.cs
@@ -1,48 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
 
 
 public class S_UIConsoleText : MonoBehaviour
 {
-    /*
-    public TextMeshProUGUI logTextPrefab; // Reference to TextMeshProUGUI prefab
-    public Transform logPanel; // Reference to the parent panel where you want to instantiate the logs
-    public float logSpacing = 5f; // Vertical spacing between log messages
+    [SerializeField]
+    private List<TextMeshProUGUI> logTextFields = new List<TextMeshProUGUI>();
+
+    [SerializeField]
+    private List<TextMeshProUGUI> logHourTextFields = new List<TextMeshProUGUI>();
+
+
+    private int currentIndex;
+    private int previousIndex;
 
     void OnEnable()
     {
         Application.logMessageReceived += LogCallback;
-    }
-
-    void OnDisable()
-    {
-        Application.logMessageReceived -= LogCallback;
-    }
-
-    void LogCallback(string logString, string stackTrace, LogType type)
-    {
-        // Create a new TextMeshProUGUI element dynamically
-        TextMeshProUGUI newLogText = Instantiate(logTextPrefab, logPanel);
-
-        // Set the log message to the new text element
-        newLogText.text = logString;
-
-        // Adjust the position of the new log element based on its index and spacing
-        RectTransform rt = newLogText.GetComponent<RectTransform>();
-        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, -logPanel.childCount * (rt.sizeDelta.y + logSpacing));
-    }*/
-
-    public TextMeshProUGUI logTextPrefab; // Reference to TextMeshProUGUI prefab
-    public Transform logPanel; // Reference to the parent panel where you want to instantiate the logs
-    public float logSpacing = 5f; // Vertical spacing between log messages
-
-    private HashSet<string> loggedMessages = new HashSet<string>();
-
-    void OnEnable()
-    {
-        Application.logMessageReceived += LogCallback;
+        currentIndex = 0;
+        previousIndex = 4; //prevent the first text from being white
     }
 
     void OnDisable()
@@ -51,43 +30,21 @@ public class S_UIConsoleText : MonoBehaviour
     }
     void LogCallback(string logString, string stackTrace, LogType type)
     {
-        // Check if the message has already been logged
-        if (loggedMessages.Contains(logString))
+        logHourTextFields[currentIndex].text = System.DateTime.Now.ToString();
+        logHourTextFields[currentIndex].color = new Color(0.153f, 0.161f, 0.42f); 
+
+        logTextFields[currentIndex].text = logString;
+        logTextFields[currentIndex].color = new Color(0.153f, 0.161f, 0.42f);
+
+        logTextFields[previousIndex].color = new Color(0.153f, 0.161f, 0.42f, 0.741f);
+        logHourTextFields[previousIndex].color = new Color(0.153f, 0.161f, 0.42f, 0.741f);
+
+        previousIndex = currentIndex;
+        currentIndex++;
+
+        if(currentIndex == logTextFields.Count)
         {
-            return;
+            currentIndex = 0;
         }
-
-        // Add the message to the set of logged messages
-        loggedMessages.Add(logString);
-
-        // Create a new TextMeshProUGUI element dynamically
-        TextMeshProUGUI newLogText = Instantiate(logTextPrefab, logPanel);
-
-        // Set the log message to the new text element
-        newLogText.text = logString;
-
-        // Adjust the position of the new log element based on its index and spacing
-        RectTransform rt = newLogText.GetComponent<RectTransform>();
-        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, -logPanel.childCount * (rt.sizeDelta.y + logSpacing));
     }
 }
-
-/*
-public TextMeshProUGUI logText;
-void OnEnable()
-{
-    Application.logMessageReceived += LogCallback;
-}
-
-void OnDisable()
-{
-    Application.logMessageReceived -= LogCallback;
-}
-
-void LogCallback(string logString, string stackTrace, LogType type)
-{
-    logText.text = logString;
-    //Or Append the log to the old one
-    //logText.text += logString + "\r\n";
-}
-*/
