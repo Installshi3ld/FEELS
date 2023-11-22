2ec19771f6760a5f3530952526096afbdd549490
DeepCalvaire <bellecourmarieamelie@gmail.com>
2023-11-22 08:41:15 +0100

    Begins of New Event System - Requirements

---
+++ Assets/User/Marie-Amélie/Scripts/S_Requirement.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Requirement : ScriptableObject
+{
+    public bool hasBeenFulfilled;
+    //public abstract void CheckIsRequirementFulfilled();
+}
