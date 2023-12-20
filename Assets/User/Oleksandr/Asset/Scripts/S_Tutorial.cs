4d2a7069906d37abb7ee1779e4918879dbd9b005
Fespaww <125281217+Fespaww@users.noreply.github.com>
2023-12-20 11:15:41 +0100

    0IQ Version of tutorial

--- Assets/User/Oleksandr/Asset/Scripts/S_Tutorial.cs
+++ Assets/User/Oleksandr/Asset/Scripts/S_Tutorial.cs
@@ -1,19 +1,43 @@
+using Sirenix.OdinInspector.Editor.GettingStarted;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class S_Tutorial : MonoBehaviour
 {
+    public GameObject[] Tutorial;
+    public bool tutorialEnabled = false;
+    public int currentTutorial = 0;
 
-    public S_TutorialInfo[] Tutorial;
+    // Start is called before the first frame update
     void Start()
     {
+
         
 
 
- 
+
+        
 
     }
 
-    
+    public void tutorialSwitch()
+    {
+        Tutorial[currentTutorial].SetActive(false);
+        currentTutorial++;
+        Tutorial[currentTutorial].SetActive(true);
+
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
