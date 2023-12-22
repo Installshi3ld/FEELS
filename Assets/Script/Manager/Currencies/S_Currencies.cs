676bf21da2854df2314757691ff2b949c8e47dfa
DeepCalvaire <bellecourmarieamelie@gmail.com>
2023-12-22 15:57:59 +0100

    Adding some UI Price of Life Experience

--- Assets/Script/Manager/Currencies/S_Currencies.cs
+++ Assets/Script/Manager/Currencies/S_Currencies.cs
@@ -28,7 +28,7 @@ public class S_Currencies : ScriptableObject
     [SerializeField]
     public FeelType feelType;
 
-   public int amount = 0;
+   private int amount = 0;
     public int Amount
     {
         get { return amount; } //read
