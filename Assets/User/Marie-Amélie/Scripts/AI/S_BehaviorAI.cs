9fdf2fcf54d5c4baca834992ebf282fe01cb8eba
Maxence PARFAIT <maxence.parfait@particeep.com>
2024-01-25 23:56:30 +0100

    marifékakapartou

--- Assets/User/Marie-Amélie/Scripts/AI/S_BehaviorAI.cs
+++ Assets/User/Marie-Amélie/Scripts/AI/S_BehaviorAI.cs
@@ -22,7 +22,20 @@ public class S_BehaviorAI : MonoBehaviour
     private void Start()
     {
         StartCoroutine(EndingMyOwnSuffering());
+		StartCoroutine(DoFadeInAndOut());
     }
+	
+	private IEnumerator DoFadeInAndOut()
+	{
+		var renderer = gameObject.GetComponent<SpriteRenderer>();
+		var color = renderer.color;
+		color.a = 0f;
+		renderer.color = color;
+
+		renderer.DOFade(1.0f, 2.5f);
+		yield return new WaitForSeconds(2.5f);
+		renderer.DOFade(0f, 2.5f);
+	}
 
     private void Update()
     {
