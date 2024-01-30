851224e0cd0f21e43faf4663d5d2a14294ff93c0
DeepCalvaire <bellecourmarieamelie@gmail.com>
2024-01-30 08:53:16 +0100

    Round creation

---
+++ Assets/User/Marie-Amélie/Scripts/Rounds/S_ScriptableRounds.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_Rounds", menuName = "SingletonContainer/Rounds")]
+public class S_ScriptableRounds : ScriptableObject
+{
+    [SerializeField] private int numberOfRoundToSwitchEvent;
+    private int currentRound;
+
+    public Action OnChangedRound;
+
+    public void SwitchRound()
+    {
+        currentRound++;
+        CheckChangeEvent();
+    }
+
+    private void CheckChangeEvent()
+    {
+        //
+    }
+}
