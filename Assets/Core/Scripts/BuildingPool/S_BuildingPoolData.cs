cf2aae3687e6a1f161f1b0aebbac692d89f4dd20
Adrien <97459378+Nivany@users.noreply.github.com>
2024-03-22 18:23:24 +0100

    Building Pool button appearing according to certain number of rounds
    Still issue because List don't reset and you have to turn false each building.
    + Issue when spawning Theme building and not one by one anymore, modify the building in it

--- Assets/Core/Scripts/BuildingPool/S_BuildingPoolData.cs
+++ Assets/Core/Scripts/BuildingPool/S_BuildingPoolData.cs
@@ -2,24 +2,50 @@
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+public struct BuildingPoolStruct 
+    {
+       public GameObject building;
+       public bool showInUI;
+        
+    public BuildingPoolStruct(bool bo, GameObject go)
+    {
+        building = go;
+        showInUI = bo;
+    }
+        public void ChanggeShowInUIStatement(bool _statement)
+        {
+            showInUI = _statement;
+            Debug.Log("_statement = "+ _statement);
+        }
 
+    }
 
 [CreateAssetMenu(fileName = "SO_BuildingPoolData", menuName = "Data/SO_BuildingPoolData")]
 public class S_BuildingPoolData : SerializedScriptableObject
 {
-    public struct BuildingPoolStruct 
-    {
-       public GameObject building;
-       public bool showInUI;
-
-    }
-
+    
     [OdinSerialize, InfoBox("Each list in Building pool is consider as Tier, based on index")]
     public List<List<BuildingPoolStruct>> list;
 
     //Adrien 
+
+    public void Awake()
+    {
+        
+        BuildingPoolStruct defaultStruct = new BuildingPoolStruct(false, null);
+
+        foreach (List<BuildingPoolStruct> innerList in list)
+        {
+            foreach (int i in Enumerable.Range(0, innerList.Count))
+            {
+                // Affecter la nouvelle instance de BuildingPoolStruct à chaque élément de la liste
+                innerList[i] = defaultStruct;
+            }
+        }
+    }
 
     public void SetShowInUI(int tierIndex, int elementIndex, bool showInUI)
     {
--- Assets/User/Adrien/Script/S_Tuto/S_EndTurnCount.cs
+++ Assets/User/Adrien/Script/S_Tuto/S_EndTurnCount.cs
@@ -7,14 +7,20 @@ using UnityEngine.UI;
 public class S_EndTurnCount : MonoBehaviour
 {
     public int endTurnCount;
-    private int valueAnimal,valueMusic,valueFood,valueActivity;
-    private int valueFaceSad, valueFaceFear,valueFaceAnger;
-    private S_BuildingPoolData buildingPoolData;
-
+    public int valueAnimal,valueMusic,valueFood,valueActivity;
+    public int valueFaceSad, valueFaceFear,valueFaceAnger;
+    public S_BuildingPoolData buildingPoolData;
+    public S_BuildingPoolUI poolUI;
     // Start is called before the first frame update
-    void Start()
+    int x = 0;
+    int y = 1;
+
+    void Awake()
     {
         endTurnCount = 0;
+        BuildingPoolStruct ms = buildingPoolData.list[0][0];
+        ms.showInUI = true;
+
     }
 
     public void EndTurnPressed()
@@ -25,46 +31,88 @@ public class S_EndTurnCount : MonoBehaviour
 
     public void ActivateButton()
     {
+        endTurnCount++;
+        Debug.Log(endTurnCount.ToString());
+        Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI Le mauvais : " + buildingPoolData.list[0][1].showInUI);
+        BuildingPoolStruct ms = buildingPoolData.list[x][y];
+        Debug.Log(buildingPoolData.list[x][y].building);
+
         if (endTurnCount == valueFaceSad)
         {
-            // Check if the list has any elements
-            if (buildingPoolData.list.Count > 0)
-            {
-                // Check if the sub-list at index 0 has any elements
-                if (buildingPoolData.list[0].Count > 1)
-                {
+    
+                    
                     // Retrieve the element at position [0][1] and modify its showInUI property
-                  //  buildingPoolData.list[0][1].showInUI = true;
-                }
-                else
-                {
-                    Debug.LogError("Sub-list at index 0 doesn't have enough elements.");
-                }
-            }
-            else
-            {
-                Debug.LogError("Main list doesn't have any elements.");
-            }
+                    //buildingPoolData.list[x][y].ChanggeShowInUIStatement(true);
+                    ms.showInUI = true;
+                    buildingPoolData.list[x][y] = ms;
+                    Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+                    y++;
+
+                
         }
 
         if (endTurnCount == valueFaceFear)
         {
-
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            y++;
         }
 
         if(endTurnCount == valueFaceAnger)
         {
-
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            x++;
+            y = 0;
         }
 
         if( endTurnCount == valueAnimal)
         {
-
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            buildingPoolData.list[x][1] = ms;
+            buildingPoolData.list[x][2] = ms;
+            buildingPoolData.list[x][3] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            x++;
         }
 
         if(endTurnCount == valueMusic)
         {
-
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            buildingPoolData.list[x][1] = ms;
+            buildingPoolData.list[x][2] = ms;
+            buildingPoolData.list[x][3] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            x++;
         }
+
+        if (endTurnCount == valueFood)
+        {
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            buildingPoolData.list[x][1] = ms;
+            buildingPoolData.list[x][2] = ms;
+            buildingPoolData.list[x][3] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            x++;
+        }
+
+        if (endTurnCount == valueActivity)
+        {
+            ms.showInUI = true;
+            buildingPoolData.list[x][y] = ms;
+            buildingPoolData.list[x][1] = ms;
+            buildingPoolData.list[x][2] = ms;
+            buildingPoolData.list[x][3] = ms;
+            Debug.Log(buildingPoolData.list[0][1].building + "ShowInUI : " + ms.showInUI);
+            x++;
+        }
+
+        poolUI.RefreshUI();
+        Debug.Log("Je refresh bg");
     }
 }
