1e9e31aaef691b38ebce2a20f270a2478974b589
pierrehale <pierrehale01@gmail.com>
2024-04-09 13:12:59 +0200

    UI tweaks and updates

--- Assets/User/Adrien/Script/UI_Currencies_Production/S_UI_HoverEndTurnButton.cs
+++ Assets/User/Adrien/Script/UI_Currencies_Production/S_UI_HoverEndTurnButton.cs
@@ -3,12 +3,34 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class UIHoverEndTurnButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class UIHoverEndTurnButton : MonoBehaviour//, IPointerEnterHandler//, IPointerExitHandler
 {
     public TextMeshProUGUI infoProductionComming; // Reference to the UI Text component to display information
     public Image productionComming;
     public S_BuildingList buildingList;
-    public void OnPointerEnter(PointerEventData eventData)
+    /*public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (buildingList != null)
+        {
+            float totalValue = 0f;
+
+            foreach (S_BuildingData buildingInfo in buildingList.builidingsInfos)
+            {
+                totalValue += buildingInfo.building._FeelAssignationBuilding.currentProduction;
+            }
+
+            // Display information when the pointer enters the UI element
+            infoProductionComming.text = "+ " + totalValue.ToString() + " Feels";
+            productionComming.gameObject.SetActive(true); // Show the text display
+        }
+        else
+        {
+            Debug.LogError("BuildingInfo is not assigned!");
+        }
+
+    }*/
+
+    void Update()
     {
         if (buildingList != null)
         {
@@ -30,9 +52,9 @@ public class UIHoverEndTurnButton : MonoBehaviour, IPointerEnterHandler, IPointe
 
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    /*public void OnPointerExit(PointerEventData eventData)
     {
         // Hide information when the pointer exits the UI element
         productionComming.gameObject.SetActive(false); // Hide the text display
-    }
+    }*/
 }
